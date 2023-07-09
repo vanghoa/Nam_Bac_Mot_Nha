66a08253ab42d109b1fa87e9cffbe9ce6e0f2343
bao ann <baoanh1buinguyen@gmail.com>
2023-07-09 20:19:04 +0700

    fixerr

--- app/api/doc/route.ts
+++ app/api/doc/route.ts
@@ -1,29 +1,29 @@
-import { google } from "googleapis";
-import { NextResponse } from "next/server";
+import { google } from 'googleapis';
+import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
-  try {
-    const client = new google.auth.JWT(
-      process.env.GOOGLE_CLIENT_EMAIL,
-      null,
-      process.env.GOOGLE_PRIVATE_KEY,
-      ["https://www.googleapis.com/auth/documents.readonly"]
-    );
+    try {
+        const client = new google.auth.JWT(
+            process.env.GOOGLE_CLIENT_EMAIL,
+            undefined,
+            process.env.GOOGLE_PRIVATE_KEY,
+            ['https://www.googleapis.com/auth/documents.readonly']
+        );
 
-    const gdapi = google.docs({ version: "v1", auth: client });
-    const opt = {
-      documentId: process.env.DOC_ID,
-    };
+        const gdapi = google.docs({ version: 'v1', auth: client });
+        const opt = {
+            documentId: process.env.DOC_ID,
+        };
 
-    let data = await gdapi.documents.get(opt);
+        let data = await gdapi.documents.get(opt);
 
-    return NextResponse.json({
-      message: data.data,
-    });
-  } catch (e) {
-    return NextResponse.json(
-      { message: JSON.stringify({ error: true, message: e.message }) },
-      { status: 400 }
-    );
-  }
+        return NextResponse.json({
+            message: data.data,
+        });
+    } catch (e) {
+        return NextResponse.json(
+            { message: JSON.stringify({ error: true, message: e.message }) },
+            { status: 400 }
+        );
+    }
 }
