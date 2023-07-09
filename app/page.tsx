1a873e7f64d800ec19f4608e659f1ef562774fed
bao ann <baoanh1buinguyen@gmail.com>
2023-07-10 02:14:36 +0700

    bipquabip

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const loaderProp = ({ src }: { src: string }) => {
 };
 
 async function getData() {
-    const res = await fetch(`https://${process.env.VERCEL_URL}/api/doc/`, {
+    const res = await fetch(`https://${process.env.VERCEL_URL}/api/doc`, {
         //cache: 'no-store',
     });
     try {
